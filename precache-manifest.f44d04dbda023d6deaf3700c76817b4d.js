8edd891456bac9b76a78d1935d38bef6533078fd
wmctavish <wbmctavish@gmail.com>
2019-02-20 14:38:50 -0500

    Updates

---
+++ precache-manifest.f44d04dbda023d6deaf3700c76817b4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f389304abcaa80d3c9b0",
+    "url": "/stockdash/static/js/runtime~main.f389304a.js"
+  },
+  {
+    "revision": "d2d7fc1fe246b91ae445",
+    "url": "/stockdash/static/js/main.d2d7fc1f.chunk.js"
+  },
+  {
+    "revision": "a5caa88e975c86242878",
+    "url": "/stockdash/static/js/2.a5caa88e.chunk.js"
+  },
+  {
+    "revision": "d2d7fc1fe246b91ae445",
+    "url": "/stockdash/static/css/main.4b3a9a28.chunk.css"
+  },
+  {
+    "revision": "71686da5137e410931394f66fda3a633",
+    "url": "/stockdash/index.html"
+  }
+];
